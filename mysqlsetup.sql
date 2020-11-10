42112c3c55d7ac6e0c8bc813c70a7b8caf0913f1
SBSpecial <70706093+SBSpecial@users.noreply.github.com>
2020-11-10 06:37:39 -0500

    Update mysqlsetup.sql

--- mysqlsetup.sql
+++ mysqlsetup.sql
@@ -1,6 +1,6 @@
+  
 USE mysql;
 ALTER user 'root'@'localhost' IDENTIFIED BY 'W3lcome!1';
 
-CREATE DATABASE wpdp;
 CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'W3lcome!1';
 GRANT ALL PRIVILEGES ON wpdb.* TO 'wpuser'@'localhost';
